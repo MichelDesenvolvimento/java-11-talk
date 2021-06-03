ca114fb074a3da01ae44d1a9cd4982d0684f773f
Michel Ribeiro Cintra Fernandez <michel.desenvolvimentoti@gmail.com>
2021-06-02 22:36:40 -0300

    Removendo comentários

--- src/main/java/java11/Java11.java
+++ src/main/java/java11/Java11.java
@@ -1,56 +1,63 @@
 package java11;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.concurrent.CompletableFuture;
 
 public class Java11 {
 
     public static void main(String[] args) throws IOException {
 
         // Métodos String
-//        var teste = "  TESTE  ";
-//        System.out.println(teste.isBlank());
-//        System.out.println(teste.lines().count());
-//        System.out.println(teste.strip());
-//        System.out.println(teste.stripLeading());
-//        System.out.println(teste.stripTrailing());
-//        System.out.println(teste.repeat(3));
+        var teste = "  TESTE  ";
+        System.out.println(teste.isBlank());
+        System.out.println(teste.lines().count());
+        System.out.println(teste.strip());
+        System.out.println(teste.stripLeading());
+        System.out.println(teste.stripTrailing());
+        System.out.println(teste.repeat(3));
 
         // Métodos de leitura e escrita de arquivos
-//        Path filePath = Paths.get("test.txt");
-//        try {
-//            if (!Files.exists(filePath))
-//                Files.createFile(filePath);
-//
-//            //Write content to file
-//            Files.writeString(filePath, "Hello World !!", StandardOpenOption.APPEND);
-//
-//            //Verify file content
-//            String content = Files.readString(filePath);
-//
-//            System.out.println(content);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        Path filePath = Paths.get("test.txt");
+        try {
+            if (!Files.exists(filePath))
+                Files.createFile(filePath);
+
+            //Write content to file
+            Files.writeString(filePath, "Hello World !!", StandardOpenOption.APPEND);
+
+            //Verify file content
+            String content = Files.readString(filePath);
+
+            System.out.println(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         // Padronização do cliente HTTP
 
-//        HttpRequest request = HttpRequest.newBuilder()
-//                .GET()
-//                .uri(URI.create("http://www.google.com.br"))
-//                .build();
-//
-//        HttpResponse.BodyHandler<String> bodyHandler = HttpResponse.BodyHandlers.ofString();
-//
-//        HttpClient client = HttpClient.newHttpClient();
-//        CompletableFuture<HttpResponse<String>> future = client.sendAsync(request, bodyHandler);
-//        future.thenApply(HttpResponse::body)
-//                .thenAccept(System.out::println)
-//                .join();
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create("http://www.google.com.br"))
+                .build();
+
+        HttpResponse.BodyHandler<String> bodyHandler = HttpResponse.BodyHandlers.ofString();
+
+        HttpClient client = HttpClient.newHttpClient();
+        CompletableFuture<HttpResponse<String>> future = client.sendAsync(request, bodyHandler);
+        future.thenApply(HttpResponse::body)
+                .thenAccept(System.out::println)
+                .join();
 
         // Módulos removidos do Java EE
+
+        // Rodar um arquivo em especifico usado o comando java
     }
 }
